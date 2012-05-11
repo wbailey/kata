2bb98187bd23ac0ed03c7e6f168ef7fbfd36e9ee
Wes Bailey <baywes@gmail.com>
2012-05-11 13:29:37 -0700

    starting kata

---
+++ calculator-2012-05-11-132937/spec/calculator_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'calculator'
+
+describe Calculator do
+  describe "new" do
+    it "should instantiate" do
+      expect {
+        Calculator.new
+      }.should_not raise_exception
+    end
+  end
+end
---
+++ calculator-2012-05-11-132937/spec/spec_helper.rb
@@ -0,0 +1,5 @@
+$: << '.' << File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'rspec'
+
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
