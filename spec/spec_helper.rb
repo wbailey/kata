1c0fb91529bc8c40aef4f53ee828506c196f5faf
Jason Noble <github+jasonn@jasonnoble.org>
2012-09-28 16:02:37 -0600

    Stub system calls so running specs does not commit stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require 'rspec'
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
+RSpec.configure do |config|
+  config.before(:each) do
+    Kata::Base.stub(:system)
+  end
+end
+
